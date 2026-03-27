6f2bfe4e457b78b795879de68109e459f62e7298
sudhanshu-pandey <101427171+sudhanshu-pandey@users.noreply.github.com>
2026-03-27 22:25:09 +0530

    Bind server to all network interfaces

--- server/src/index.js
+++ server/src/index.js
@@ -34,6 +34,6 @@ app.use("/api/reviews", reviewRouter);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
